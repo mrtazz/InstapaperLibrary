19d5d85b64268b39f4c34d9d877010914f55f67b
Daniel <d@lordofhosts.de>
2010-01-23 17:39:13 +0100

    minor style corrections

--- instapaper.py
+++ instapaper.py
@@ -1,13 +1,16 @@
-#
-# Instapaper Library written in Python. 
-#
-#
+##
+# @file instapaper.py
+# @brief simple library to use instapaper
+# @author Daniel Schauenberg <d@lordofhosts.de>
+# @version 0.1
+# @date 2010-01-23
 
 import urllib
 import urllib2
 
+
 class Instapaper:
-    ''' This class provides the structure for the connection object '''
+    """ This class provides the structure for the connection object """
 
     def __init__(self, user, password):
         self.user = user
@@ -15,11 +18,12 @@ class Instapaper:
         self.authurl = "https://www.instapaper.com/api/authenticate"
         self.addurl = "https://www.instapaper.com/api/add"
 
-    def addItem(self, url, title=""):
-        ''' Method to add a new item to a instapaper account
+    def add_item(self, url, title=""):
+        """ Method to add a new item to a instapaper account
             Returns 0 on success and -1 if something went wrong
-        '''
-        parameters = {'username' : self.user,'password' : self.password,'url' : url, 'title' : title}
+        """
+        parameters = {'username' : self.user,'password' : self.password,
+                      'url' : url, 'title' : title}
         headerdata = urllib.urlencode(parameters)
         try:
             request = urllib2.Request(self.addurl, headerdata)
