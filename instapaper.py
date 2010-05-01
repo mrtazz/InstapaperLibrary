b1843383676f177f7217bf6fcec69089410a56cc
Daniel Schauenberg <d@unwiredcouch.com>
2010-05-01 18:37:42 +0200

    minor cleanup

--- instapaper.py
+++ instapaper.py
@@ -1,13 +1,9 @@
 ##
 # @file instapaper.py
 # @brief simple library to use instapaper
-# @author Daniel Schauenberg <d@lordofhosts.de>
-# @version 0.1
-# @date 2010-01-23
 
 import urllib
 import urllib2
-
 
 class Instapaper:
     """ This class provides the structure for the connection object """
