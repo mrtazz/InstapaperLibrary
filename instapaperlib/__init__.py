114b35ccf4710e65c0fff474b4dbecc7f1631a58
Daniel Schauenberg <d@unwiredcouch.com>
2010-05-24 22:36:19 +0200

    default value for URL parameter

--- instapaperlib/__init__.py
+++ instapaperlib/__init__.py
@@ -9,7 +9,7 @@ __license__ = "MIT"
 def auth(user='', password=''):
     return Instapaper(user, password).auth()
 
-def add_item(user='', password='', url,
+def add_item(user='', password='', url=None,
              title=None, selection=None,
              response_header=False):
     return Instapaper(user, password).add_item(url,title,
